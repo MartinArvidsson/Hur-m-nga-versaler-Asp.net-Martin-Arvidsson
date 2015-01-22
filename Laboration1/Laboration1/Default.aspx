0a0e3b0895f4fa2d210725d82b14ec875044de6a
Martin Arvidsson UD14 <ma223ku@student.lnu.se>
2015-01-22 20:53:23 +0100

    Klar med labb 1 :D

---
+++ Laboration1/Laboration1/Default.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Laboration1.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Hur Många Versaler??</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div style="height: 152px">
+    <h1>Hur många versaler?</h1>
+        <asp:TextBox ID="Textbox" runat="server" Height="48px" Width="394px"></asp:TextBox>
+        <asp:Button ID="Button" runat="server" Text="Kör!" Width="106px" OnClick="Button_Click" />
+        <div>
+        <asp:Label ID="Result" runat="server"></asp:Label>    
+        </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Laboration1/Laboration1/Default.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Laboration1
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+        protected void Button_Click(object sender, EventArgs e)
+        {
+            if(Button.Text !="Starta om")
+            {
+                int Versaler = 0;
+                string Information = Textbox.Text;
+                for (int i = 0; i < Information.Length; i++)
+                {
+                    if (char.IsUpper(Information[i])) Versaler++;
+                }
+                
+                Result.Text = string.Format("Det fanns {0} Versaler", Versaler);
+                Button.Text = "Starta om";
+                Textbox.Enabled = false;
+            }
+            else
+            {
+                Button.Text = "Kör!";
+                Result.Text = "";
+                Textbox.Text = "";
+                Textbox.Enabled = true;
+            }
+        }
+    }
+}
