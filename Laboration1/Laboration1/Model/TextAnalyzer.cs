867ae2041123cd81b07db33080ccbe323a1bb43d
MartinArvidsson <ma223ku@lnu.student.se>
2015-01-26 09:56:30 +0100

    Forgot to push

---
+++ Laboration1/Laboration1/Model/TextAnalyzer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Laboration1.Model
+{
+    public static class TextAnalyzer
+    {
+        public static int NumberOfCapitals(string input)
+        {
+            int CapitalLetters = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (char.IsUpper(input[i])) CapitalLetters++;
+            }
+            return CapitalLetters;
+        }
+    }
+}
